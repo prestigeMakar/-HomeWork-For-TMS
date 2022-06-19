c2f448082c6e1d854f3a4b64f853a4a7fef1a749
prestigeMakar <prestigemakar@gmail.com>
2022-06-19 12:50:04 +0300

    Initial Commit

---
+++  HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//   HomeWork
+//
+//  Created by Артем Макаренко on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
